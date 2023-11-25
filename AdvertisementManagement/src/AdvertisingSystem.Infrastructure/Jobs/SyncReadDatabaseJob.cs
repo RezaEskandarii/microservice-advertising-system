691462fdd153e82a5e4b47635ec3e3386742f4e5
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2023-11-25 22:31:37 +0330

    feat: override dbContext saveChangeAsync method

--- AdvertisementManagement/src/AdvertisingSystem.Infrastructure/Jobs/SyncReadDatabaseJob.cs
+++ AdvertisementManagement/src/AdvertisingSystem.Infrastructure/Jobs/SyncReadDatabaseJob.cs
@@ -1,4 +1,5 @@
 using AdvertisingSystem.Domain.Entities;
+using AdvertisingSystem.Domain.ValueObjects;
 using AdvertisingSystem.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -36,7 +37,8 @@ public class SyncReadDatabaseJob : IJob
                 .Take(pageSize)
                 .ToList();
 
-            var insertResult = await InsertToElasticsearch(elasticClient, advertisementsToProcess);
+            var insertResult =
+                await InsertToElasticsearch(elasticClient, MapToAdvertisementDto(advertisementsToProcess));
             if (insertResult)
             {
                 await MarkAsSynced(_dbContext, advertisementsToProcess);
@@ -46,14 +48,14 @@ public class SyncReadDatabaseJob : IJob
 
     #region Private
 
-    private async Task<bool> InsertToElasticsearch(ElasticClient elasticClient, List<Advertisement> data)
+    private async Task<bool> InsertToElasticsearch(ElasticClient elasticClient, ICollection<AdvertisementDto> data)
     {
         var indexName = "advertisements";
         var bulkDescriptor = new BulkDescriptor();
 
         foreach (var entity in data)
         {
-            bulkDescriptor.Index<Advertisement>(i => i
+            bulkDescriptor.Index<AdvertisementDto>(i => i
                 .Index(indexName)
                 .Document(entity));
         }
@@ -73,6 +75,27 @@ public class SyncReadDatabaseJob : IJob
         await dbContext.SaveChangesAsync();
     }
 
+    private ICollection<AdvertisementDto> MapToAdvertisementDto(List<Advertisement> data)
+    {
+        return data.Select(advertisement => new AdvertisementDto
+            {
+                Id = advertisement.Id,
+                Title = advertisement.Title,
+                UserId = advertisement.UserId,
+                Description = advertisement.Description,
+                Price = advertisement.Price?.Amount,
+                ExpiresAt = advertisement.ExpiresAt.Value,
+                CreatedAt = advertisement.CreatedAt.Value,
+                UpdatedAt = advertisement.UpdatedAt.Value,
+                Address = advertisement.Address,
+                CategoryId = advertisement.CategoryId,
+                Thumbnails = advertisement.Thumbnails,
+                Properties = advertisement.Properties.ToDictionary(x => x.Name, y => y.Value),
+                Tags = advertisement.Tags
+            })
+            .ToList();
+    }
+
     private IQueryable<Advertisement> GetUnSyncedAdvertisements(ApplicationDbContext dbContext)
     {
         return dbContext
@@ -82,4 +105,21 @@ public class SyncReadDatabaseJob : IJob
     }
 
     #endregion
+}
+
+internal class AdvertisementDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string UserId { get; set; }
+    public string Description { get; set; }
+    public decimal? Price { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Address? Address { get; set; }
+    public int CategoryId { get; set; }
+    public ICollection<string> Thumbnails { get; set; } = new List<string>();
+    public ICollection<KeyValuePair<string, string>>? Properties { get; set; }
+    public string[]? Tags { get; set; }
 }
--- AdvertisementManagement/src/AdvertisingSystem.Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ AdvertisementManagement/src/AdvertisingSystem.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -11,13 +11,31 @@ public class ApplicationDbContext : DbContext
         AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
-    
+
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfiguration(new AdvertisementConfiguration());
         base.OnModelCreating(builder);
     }
+
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var modifiedEntities = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Modified)
+            .Select(e => e.Entity)
+            .OfType<Advertisement>(); // Filter for Advertisement entities
+
+        foreach (var entity in modifiedEntities)
+        {
+            // Set isSynced to false for Advertisement entities being updated
+            entity.UpdateIsSyncedInReadDb(false);
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
 
     public DbSet<Advertisement> Advertisements { get; set; }
 }
