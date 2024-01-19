fb0f9472eb9bde32784adbe8dadecfb1a491b31f
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-01-19 22:13:27 +0330

    refactor: update identity project nuget packages

--- IdentityManagement/src/AdvertisingSystem.Identity.Application/Behaviors/ValidationBehavior.cs
+++ IdentityManagement/src/AdvertisingSystem.Identity.Application/Behaviors/ValidationBehavior.cs
@@ -1,12 +1,24 @@
+using FluentValidation;
 using MediatR;
 
 namespace AdvertisingSystem.Identity.Application.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class ValidationBehavior<TRequest, TResponse>: IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
+    
     {
+ 
+    
+        
         var resp = await next();
 
         return resp;
