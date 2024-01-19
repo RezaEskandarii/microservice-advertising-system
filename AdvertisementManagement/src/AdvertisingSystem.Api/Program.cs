2f6169c21d26fc1aeda374d616805eb24cc0494a
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-01-19 19:22:39 +0330

    feat: add healthcheck to advertisement management project

--- AdvertisementManagement/src/AdvertisingSystem.Api/Program.cs
+++ AdvertisementManagement/src/AdvertisingSystem.Api/Program.cs
@@ -1,6 +1,9 @@
 using AdvertisingSystem.Application;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -8,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices(builder.Configuration);
- 
+builder.Services.AddHealthChecks().AddNpgSql(connStr ?? "");
 
 var app = builder.Build();
 
@@ -22,5 +25,15 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
- 
+
+app.UseRouting()
+    .UseEndpoints(config =>
+    {
+        config.MapHealthChecks("healthcheck", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    });
+
 app.Run();
