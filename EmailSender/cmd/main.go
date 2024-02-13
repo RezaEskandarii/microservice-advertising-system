1993847551a72223cb8401591273d04d27fe56e6
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-02-13 18:28:25 +0330

    refactor: remove some extra comments

--- EmailSender/cmd/main.go
+++ EmailSender/cmd/main.go
@@ -25,9 +25,7 @@ func main() {
 
 	// Listen for a termination signal
 	quit := make(chan os.Signal, 1)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 
