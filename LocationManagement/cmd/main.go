d7eebb6b735b2c9d713d8f73f4bf8ad2c677464f
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-03-17 10:16:34 +0330

    doc: add comment to main method in locationManagement microservice

--- LocationManagement/cmd/main.go
+++ LocationManagement/cmd/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
+	//Get the port number from the environment variable
 	port := env_manager.GetFromDotENV("location_app_port")
 
+	//Run the application
 	if err := application.Run(port); err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err.Error()) // Log the error and exit the program
 	}
 }
