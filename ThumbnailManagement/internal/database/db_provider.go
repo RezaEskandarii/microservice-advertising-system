8a26491e5ef699668460110d1021c5e6625c13cb
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-03-22 11:37:45 +0330

    doc: add comment to GetDb method

--- ThumbnailManagement/internal/database/db_provider.go
+++ ThumbnailManagement/internal/database/db_provider.go
@@ -7,13 +7,15 @@ import (
 	"thumbnail-management/pkg/env_manager"
 )
 
+// GetDb retrieves a connection to the database using the provided context.
 func GetDb(ctx context.Context) *sql.DB {
-
 	sdn := env_manager.Load("thumbnail_management_full_db")
 
+	// Open a connection to the PostgreSQL database using the connection string.
 	db, err := sql.Open("postgres", sdn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return db
 }
