afbcbdd22473975b6635e43f80ea6d3162e305a5
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-03-24 17:48:05 +0330

    refactor: clean some codes

--- AdvertisementManagement/src/AdvertisingSystem.Application/Handlers/QueryHandlers/SearchAdvertisementQueryHandler.cs
+++ AdvertisementManagement/src/AdvertisingSystem.Application/Handlers/QueryHandlers/SearchAdvertisementQueryHandler.cs
@@ -18,11 +18,9 @@ public class SearchAdvertisementQueryHandler : IRequestHandler<SearchAdvertiseme
         _advertisementRepository = advertisementRepository;
     }
 
-    public async Task<PaginatedList<GetAdvertisementViewModel>> Handle(SearchAdvertisementQuery request,
-        CancellationToken cancellationToken)
+    public async Task<PaginatedList<GetAdvertisementViewModel>> Handle(SearchAdvertisementQuery request, CancellationToken cancellationToken)
     {
         var advertisements = await _advertisementRepository.SearchAsync(request.PageNumber,request.PageSize,request.Filter,request.Properties);
-        
         return _mapper.Map<PaginatedList<GetAdvertisementViewModel>>(advertisements);
     }
 }
