fa1cb0182d6a745bcf0d0af74ba3cb33ab901576
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-03-30 16:03:45 +0330

    feat: call Run method in main function

---
+++ WalletApi/cmd/app/run.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	api "wallet-api/api/http"
+	"wallet-api/internal/database"
+	"wallet-api/internal/repositories"
+	"wallet-api/internal/services"
+	"wallet-api/pkg/env_manager"
+)
+
+func Run(ctx context.Context) error {
+	if err := database.Init(ctx); err != nil {
+		return err
+	}
+
+	db, err := database.GetWalletApiDB(ctx)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	walletRepository := repositories.NewPostgreSQLRepository(db)
+	walletService := services.NewWalletService(walletRepository)
+
+	handler := api.NewWalletHandler(walletService)
+	handler.InitRoutes()
+
+	port := env_manager.LoadEnv("wallet_api_port")
+	fmt.Printf("######## wallet api started on: %s #######\n", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		return err
+	}
+
+	return nil
+}
--- WalletApi/cmd/main.go
+++ WalletApi/cmd/main.go
@@ -1,5 +1,15 @@
-package cmd
+package main
+
+import (
+	"context"
+	"log"
+	"wallet-api/cmd/app"
+)
 
 func main() {
+	ctx := context.Background()
 
+	if err := app.Run(ctx); err != nil {
+		log.Fatal(err.Error())
+	}
 }
