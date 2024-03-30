bd8e0239de64a7626b6ddfb9b0d18ab108c31818
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-03-30 16:08:48 +0330

    refactor: extract create database functionality into separated function in database package

--- WalletApi/internal/database/database.go
+++ WalletApi/internal/database/database.go
@@ -10,20 +10,16 @@ import (
 // GetPostgresDB retrieves a connection to the postgres base database using the provided context.
 func GetPostgresDB(ctx context.Context) (*sql.DB, error) {
 	sdn := env_manager.LoadEnv("postgres_connection_string")
-
-	// Open a connection to the PostgreSQL database using the connection string.
-	db, err := sql.Open("postgres", sdn)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	return getDB(sdn)
 }
 
 // GetWalletApiDB retrieves a connection to the wallet api database using the provided context.
 func GetWalletApiDB(ctx context.Context) (*sql.DB, error) {
 	sdn := env_manager.LoadEnv("wallet_api_connection_string")
+	return getDB(sdn)
+}
 
+func getDB(sdn string) (*sql.DB, error) {
 	// Open a connection to the PostgreSQL database using the connection string.
 	db, err := sql.Open("postgres", sdn)
 	if err != nil {
